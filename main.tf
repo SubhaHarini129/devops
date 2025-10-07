7c791d74d022ca9f24ba1b4a556e5902ac6a24c4
SUBHA HARINI <62604034+SubhaHarini129@users.noreply.github.com>
2025-10-07 14:48:58 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+
+  backend "azurerm" {
+    resource_group_name  = "learn-terraform-rg"
+    storage_account_name = "harinilearnterraform123"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "320fb61f-2500-4465-9cb2-ca21d99aa5b2"
+}
+module "storage" {
+source = "./modules/storage_account"
+resource_group_name = "rg-simple-tf"
+location = "eastus"
+storage_account_name = "simplestoragesubha001"
+}
